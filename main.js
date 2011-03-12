2d391b2ceb2f5a50986121bc06d2f07f1a73cec3
ChrisJan00 <cjan_ynzn@yahoo.es>
2011-03-12 19:05:23 +0100

    first impl

---
+++ main.js
@@ -0,0 +1,450 @@
+function prepareGame()
+{ 
+	GLOBAL.gameCanvas = document.getElementById("canvas1");
+	GLOBAL.gameContext = GLOBAL.gameCanvas.getContext("2d");
+	GLOBAL.bgCanvas = document.createElement('canvas');
+	GLOBAL.bgCanvas.width = GLOBAL.gameCanvas.width;
+	GLOBAL.bgCanvas.height = GLOBAL.gameCanvas.height;
+	GLOBAL.bgContext = GLOBAL.bgCanvas.getContext("2d");
+	GLOBAL.canvasWidth = GLOBAL.gameCanvas.width;
+	GLOBAL.canvasHeight = GLOBAL.gameCanvas.height;
+	
+	GLOBAL.action = {
+		turn:1,
+		selection:-1
+	};
+	//GLOBAL.board = {};
+	initBoard();
+	GLOBAL.pile = {};
+	
+	
+	// start with an empty background
+	clear();
+	drawBoard();
+	drawPile(35);
+	drawPile(505);
+	chooseTiles(1);
+	chooseTiles(2);
+	showPlayer();
+	
+	// clicking on the board
+	GLOBAL.mouse = {
+		x : 0,
+		y : 0,
+		button : false
+	};
+	GLOBAL.gameCanvas.addEventListener('mousedown', mouseDown, false);
+    GLOBAL.gameCanvas.addEventListener('mousemove', mouseMove, false);
+    GLOBAL.gameCanvas.addEventListener('mouseup',   mouseUp, false);
+	
+}
+
+function initBoard() {
+	GLOBAL.board = {};
+	for (var i=0; i<6; i++) {
+		GLOBAL.board[i] = {};
+	}
+}
+
+function chooseTiles(playernum) {
+	var pn = playernum-1;
+	if (pn<0) return;
+	if (pn>1) return;
+	GLOBAL.pile[pn] = new Array();
+	for (var i=0;i<18;i++) {
+		var st = {
+			ix : Math.floor(i/9),
+			iy : Math.floor(i%9),
+			bgColor : "#FFFFFF",
+			visible: true,
+			selected: false,
+			color: 0,
+			colorCode: 0,
+			owner : playernum
+		}
+		st.colorCode = Math.floor(Math.random()*4);
+		st.color = colorForCode(st.colorCode);
+		
+		GLOBAL.pile[pn].push(st);
+		drawStone(st, playernum);
+	}
+}
+
+function colorForCode(code) {
+	switch( code ) {
+			case 0: return "#FF0000"; break;
+			case 1: return "#00FF00"; break;
+			case 2: return "#0000FF"; break;
+			case 3: return "#FFFF00"; break;
+		}
+}
+
+function mouseDown( ev ) {
+	GLOBAL.mouse.button = true;	
+	
+	mouseMove( ev );
+	
+	if (GLOBAL.mouse.x >= 35 && GLOBAL.mouse.x <= 135 && GLOBAL.mouse.y >= 15 && GLOBAL.mouse.y <= 465)
+		clickedOnPile(1);
+	else if (GLOBAL.mouse.x >= 505 && GLOBAL.mouse.x <= 605 && GLOBAL.mouse.y >= 15 && GLOBAL.mouse.y <= 465)
+		clickedOnPile(2);
+	else if (GLOBAL.mouse.x >= 170 && GLOBAL.mouse.x<=470 && GLOBAL.mouse.y >= 90 && GLOBAL.mouse.y <= 390)
+		clickedOnBoard();
+	
+}
+
+function clickedOnPile(pilenum) {
+	var mix = Math.floor((GLOBAL.mouse.x - (pilenum==1?35:505))/50);
+	var miy = Math.floor((GLOBAL.mouse.y - 15) / 50);
+	var index = mix*9 + miy;
+
+	if (GLOBAL.action.turn != pilenum)
+		return;
+		
+	var pn = pilenum-1;
+	var oldIndex = GLOBAL.action.selection;
+	if (GLOBAL.action.selection != -1) {
+		var oldStone = GLOBAL.pile[pn][GLOBAL.action.selection];
+		if (oldStone.visible) {
+			GLOBAL.action.selection = -1;
+			oldStone.selected = false;
+			oldStone.bgColor = "#FFFFFF";
+			drawStone(oldStone, pilenum);
+		}
+	}
+	
+	if (oldIndex != index) {
+		var newStone = GLOBAL.pile[pn][index];
+		if (newStone.visible) {
+			GLOBAL.action.selection = index;
+			newStone.selected = true;
+			newStone.bgColor = colorForPlayer(pn);
+			drawStone(newStone, pilenum);
+		}
+	}
+	
+	
+	// var st = {
+	// 	ix : mix,
+	// 	iy : miy,
+	// 	color : "#FFEEAA",
+	// 	bgColor: "#334455"
+	// };
+	//drawStone(st, pilenum);
+}
+
+function clickedOnBoard() {
+	var mix = Math.floor((GLOBAL.mouse.x - 170)/50);
+	var miy = Math.floor((GLOBAL.mouse.y - 90)/50);
+	var boardIndex = mix + miy * 6;
+	
+	// no selection?
+	if (GLOBAL.action.selection == -1)
+		return;
+		
+	// place taken?
+	if (GLOBAL.board[mix][miy])
+		return;
+	
+	// undraw stone in player pile
+	var pn = GLOBAL.action.turn - 1;
+	var stone = GLOBAL.pile[pn][GLOBAL.action.selection];
+	stone.visible = false;
+	stone.bgColor = "#FFFFFF";
+	drawStone(stone, pn+1);
+	
+	// move stone to board
+	var newStone = {
+		ix: mix,
+		iy: miy,
+		bgColor : 0,
+		color: stone.color,
+		visible : true,
+		colorCode: stone.colorCode,
+		owner : stone.owner
+		};
+ 	newStone.bgColor = colorForPlayer(pn);
+	drawStone(newStone, 0);
+	GLOBAL.board[mix][miy] = newStone;
+	
+	startFlood(mix, miy);
+	
+	GLOBAL.action.selection = -1;
+	GLOBAL.action.turn = 3-GLOBAL.action.turn;
+	showPlayer();
+	
+	// var st = {
+	// 	ix : mix,
+	// 	iy : miy,
+	// 	color : "#FFEEAA",
+	// 	bgColor: "#334455"
+	// };
+	// drawStone(st, 0);
+}
+
+function mouseUp( ev ) {
+	GLOBAL.mouse.button = false;
+}
+
+function mouseMove( ev ) {
+	if (ev.layerX || ev.layerX == 0) { // Firefox
+    	GLOBAL.mouse.x = ev.layerX - 8;
+    	GLOBAL.mouse.y = ev.layerY - 8;
+  } else if (ev.offsetX || ev.offsetX == 0) { // Opera
+    	GLOBAL.mouse.x = ev.offsetX - 8;
+    	GLOBAL.mouse.y = ev.offsetY - 8;
+  }
+
+}
+
+function clear() {
+	GLOBAL.gameContext.fillStyle = "#FFFFFF";
+	GLOBAL.gameContext.fillRect(0, 0, GLOBAL.gameCanvas.width, GLOBAL.gameCanvas.height);
+	
+}
+function drawBoard()
+{
+	var x0 = 170;
+	var y0 = 90;
+	var side = 50;
+	var ctx = GLOBAL.gameContext;
+	ctx.strokeStyle = "#000000"
+	for (var i=0;i<7;i++) {
+		ctx.beginPath();
+		ctx.moveTo(x0 + i*side, y0);
+		ctx.lineTo(x0 + i*side, y0 + side*6);
+		ctx.stroke();
+		
+		ctx.beginPath();
+		ctx.moveTo(x0, y0+i*side);
+		ctx.lineTo(x0 + side*6, y0+i*side);
+		ctx.stroke();
+	}
+}
+
+function drawPile(x0) 
+{
+	var y0 = 15;
+	var side = 50;
+	var ctx = GLOBAL.gameContext;
+	ctx.strokeStyle = "#000000"
+	for (var i=0;i<10;i++) {
+		ctx.beginPath();
+		ctx.moveTo(x0, y0+i*side);
+		ctx.lineTo(x0 + side*2, y0+i*side);
+		ctx.stroke();
+	}
+	
+	for (var i=0;i<3;i++) {
+		ctx.beginPath();
+		ctx.moveTo(x0 + i*side, y0);
+		ctx.lineTo(x0 + i*side, y0 + side*9);
+		ctx.stroke();
+	}
+}
+
+function drawStone(stone, where) {
+	var x0, y0, ix, iy;
+	switch (where) {
+		case 0: { // board
+			x0 = 170;
+			y0 = 90;
+			break;
+		}
+		case 1: { // pile left
+			x0 = 35;
+			y0 = 15;
+			break;
+		}
+		case 2: { // pile right
+			x0 = 505;
+			y0 = 15;
+			break;
+		}
+	}
+	
+	ix = stone.ix;
+	iy = stone.iy;
+	
+	// draw background
+	var ctx = GLOBAL.gameContext;
+	ctx.fillStyle = stone.bgColor;
+	ctx.strokeStyle = "#000000";
+	ctx.beginPath();
+	ctx.moveTo(x0 + ix*50, y0+iy*50);
+	ctx.lineTo(x0 + ix*50 + 50, y0+iy*50);
+	ctx.lineTo(x0 + ix*50 + 50, y0+iy*50+50);
+	ctx.lineTo(x0 + ix*50, y0+iy*50+50);
+	ctx.closePath();
+	ctx.fill();
+	ctx.stroke();
+	
+	if (stone.visible) {
+		// draw stone
+		ctx.fillStyle = stone.color;
+		ctx.beginPath();
+		ctx.arc(ix*50 + x0 + 25,iy*50 + y0 + 25, 22, 0, Math.PI*2,true);
+		ctx.closePath();
+		ctx.fill();
+		ctx.stroke();
+	}
+}
+function colorForPlayer(pn) {
+	return pn?"#FF8C00":"#9932CC";
+}
+function showPlayer() {
+	var ctx = GLOBAL.gameContext;
+	ctx.fillStyle = "#FFFFFF";
+	ctx.fillRect(170,0,300,89);
+	var pn = GLOBAL.action.turn - 1;
+	ctx.font = "bold 24px sans-serif";
+	ctx.fillStyle = colorForPlayer(pn);
+	var msg = "Player "+(pn?"orange":"purple")+"'s turn";
+	var msglen = ctx.measureText(msg);
+	ctx.fillText(msg, 320 - msglen.width/2, 45 );
+}
+
+function startGame()
+{
+	prepareGame();
+}
+// gravityDir:
+// 0: up
+// 1: down
+// 2: right
+// 3: left
+
+//---------------------------------------------------------------------------------------------
+function startFlood(ix, iy) {
+	GLOBAL.floodFill = {}
+	for (var i=0;i<6;i++) {
+		GLOBAL.floodFill[i] = {};
+		for (var j=0; j<6; j++)
+			GLOBAL.floodFill[i][j] = true;
+	}
+	
+	checkTile(ix, iy);
+	
+	
+}
+
+function checkTile(ix, iy) {
+	
+	var stone = GLOBAL.board[ix][iy];
+	if (!stone)
+		return;
+		
+	var attackPile = new Array();
+	var defensePile = new Array();
+	
+	// left
+	if (ix>0 && GLOBAL.board[ix-1][iy] && GLOBAL.floodFill[ix-1][iy]) {
+		if (tileWinsTile(stone.colorCode, GLOBAL.board[ix-1][iy].colorCode)) {
+			attackPile.push(GLOBAL.board[ix-1][iy]);
+		} else if (tileWinsTile(GLOBAL.board[ix-1][iy].colorCode, stone.colorCode)) {
+			defensePile.push(GLOBAL.board[ix-1][iy]);
+		}
+	}
+	
+	
+	// right
+	if (ix<5 && GLOBAL.board[ix+1][iy] && GLOBAL.floodFill[ix+1][iy]) {
+		if (tileWinsTile(stone.colorCode, GLOBAL.board[ix+1][iy].colorCode)) {
+			attackPile.push(GLOBAL.board[ix+1][iy]);
+		} else if (tileWinsTile(GLOBAL.board[ix+1][iy].colorCode, stone.colorCode)) {
+			defensePile.push(GLOBAL.board[ix+1][iy]);
+		}
+	}
+	
+	// up
+	if (iy>0 && GLOBAL.board[ix][iy-1] && GLOBAL.floodFill[ix][iy-1]) {
+		if (tileWinsTile(stone.colorCode, GLOBAL.board[ix][iy-1].colorCode)) {
+			attackPile.push(GLOBAL.board[ix][iy-1]);
+		} else if (tileWinsTile(GLOBAL.board[ix][iy-1].colorCode, stone.colorCode)) {
+			defensePile.push(GLOBAL.board[ix][iy-1]);
+		}
+	}
+	
+	// down
+	if (iy<5 && GLOBAL.board[ix][iy+1] && GLOBAL.floodFill[ix][iy+1]) {
+		if (tileWinsTile(stone.colorCode, GLOBAL.board[ix][iy+1].colorCode)) {
+			attackPile.push(GLOBAL.board[ix][iy+1]);
+		} else if (tileWinsTile(GLOBAL.board[ix][iy+1].colorCode, stone.colorCode)) {
+			defensePile.push(GLOBAL.board[ix][iy+1]);
+		}
+	}
+	
+	var sleepDelay = 50;
+	var toConvert = new Array();
+	
+	// attack: convert each tile
+	for (var i=0; i<attackPile.length; i++) {
+		if (GLOBAL.floodFill[attackPile[i].ix][attackPile[i].iy])
+			toConvert.push(attackPile[i]);
+		convertStone( stone, attackPile[i] );
+	}
+	
+	// defense: first find an own tile for defense
+	var wasConverted = false;
+	for (var i=0; i<defensePile.length; i++) {
+		if (defensePile[i].owner == stone.owner) {
+			if (GLOBAL.floodFill[stone.ix][stone.iy])
+				toConvert.push(stone);
+				convertStone( defensePile[i], stone );
+			wasConverted = true;
+			break;
+		}
+	}
+	// if not, an enemy tile
+	if (!wasConverted) {
+		for (var i=0; i<defensePile.length; i++) {
+			if (defensePile[i].owner != stone.owner) {
+				if (GLOBAL.floodFill[stone.ix][stone.iy])
+					toConvert.push(stone);
+				convertStone( defensePile[i], stone );
+				break;
+			}
+		}
+	}
+	
+	// and now expand
+	for (var i=0; i<toConvert.length; i++)
+		checkTile(toConvert[i].ix,toConvert[i].iy);
+}
+
+function tileWinsTile(colorAtk, colorDef) {
+	if (colorAtk == 0 && colorDef == 3) return true;
+	if (colorAtk == 1 && colorDef == 2) return true;
+	if (colorAtk == 2 && colorDef == 0) return true;
+	if (colorAtk == 3 && colorDef == 1) return true;
+	return false;
+}
+
+function convertStone(from, to) {
+	if (!GLOBAL.floodFill[from.ix][from.iy])
+		return;
+		
+	to.colorCode = from.colorCode;
+	to.color = from.color;
+	to.owner = from.owner;
+	to.bgColor = from.bgColor;
+	GLOBAL.floodFill[from.ix][from.iy] = false;
+	
+	drawStone(to, 0);
+}
+
+//---------------------------------------------------------------------------------------------
+
+// Use this function to update the simulation.  dt will be the same as gameControl.updateStep, given in ms
+function update(dt) 
+{
+	
+}
+
+// Use this function to update the graphics, using the game state computed in "update".  dt is given in milliseconds and represents
+// elapsed time since the last call to dt.  Use it to interpolate the graphics and achieve a smoother simulation, although you can
+// safely ignore it if you want (no interpolation at all).
+function draw(dt)
+{ 
+	
+}
